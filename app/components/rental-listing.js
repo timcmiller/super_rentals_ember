d344c522ad6581adf3cda90ecca04f2d004506e4
timcmiller <timcmiller@live.com>
2016-02-25 13:12:33 -0800

    handlebars helper

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isImageShowing: false,
+  action: {
+    imageShow() {
+      this.set('isImageShowing', true);
+    },
+    imageHide() {
+      this.set('isImageShowing', false);
+    }
+  }
 });
